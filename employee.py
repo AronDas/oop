d4e29377d787d318701aa662c1516a799cae4b67
AronDas <arondas.sg@gmail.com>
2026-05-12 19:31:23 -0300

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,16 @@
+class Employee():
+
+    def __init__(self):
+        print("Employee hired")
+    def __del__(self):
+        print("Destructor called")
+    def create_object():
+        print("Hiring employee...")
+        print("Function end...")
+        return object
+    
+
+print("Calling create_Object function...")
+
+object = Employee()
+print("Function end...")
